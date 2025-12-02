9b21ac2c4e7f39d6eeda85637061c2ba6afcdb40
HectorMMZ <zacrosgamer29@gmail.com>
2025-12-02 00:21:00 -0600

    Add files via upload

---
+++ Proyecto Adivina la Palabra/Proyecto Adivina la Palabra/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Proyecto_Adivina_la_Palabra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblGuessed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(215, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adivina la Palabra";
+            // 
+            // lblWord
+            // 
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.ForeColor = System.Drawing.Color.White;
+            this.lblWord.Location = new System.Drawing.Point(217, 121);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(338, 53);
+            this.lblWord.TabIndex = 1;
+            this.lblWord.Text = "label2";
+            this.lblWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(223, 216);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(332, 45);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblInfo.Location = new System.Drawing.Point(217, 303);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(338, 53);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Palabras: 0 de 0";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGuessed
+            // 
+            this.lblGuessed.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessed.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblGuessed.Location = new System.Drawing.Point(561, 213);
+            this.lblGuessed.Name = "lblGuessed";
+            this.lblGuessed.Size = new System.Drawing.Size(338, 53);
+            this.lblGuessed.TabIndex = 1;
+            this.lblGuessed.Text = "Adivinado: 0 veces";
+            this.lblGuessed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(896, 520);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblGuessed);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Juego Adivina la Palabra";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblGuessed;
+    }
+}
+
---
+++ Proyecto Adivina la Palabra/Proyecto Adivina la Palabra/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Proyecto_Adivina_la_Palabra
+{
+    public partial class Form1 : Form
+    {
+        List<string> words = new List<string>();
+        string newText;
+        int i = 0;
+        int guessed = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Setup();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) // Si la tecla presionada es enter se ejecuta lo siguiente
+            {
+                if (words[i].ToLower() == textBox1.Text.ToLower()) // Asegurarnos que las palabras esten en minusculas
+                {
+                    if (i < words.Count - 1) // Asegurarnos de estan en los limites de la lista
+                    {
+                        //MessageBox.Show("Correcto!", "Hector dice: ");
+                        textBox1.Text = "";
+                        i++;
+                        newText = Scramble(words[i]);
+                        lblWord.Text = newText;
+                        lblInfo.Text = "Palabras: " + (i + 1) + " de " + words.Count;
+                        guessed = 0;
+                        lblGuessed.Text = "Adivinado: " + guessed + " veces.";
+                    }
+                    else
+                    {
+                        lblWord.Text = "Ganaste!, bien hecho";
+                        return;
+                    }
+                }
+                else
+                {
+                    guessed += 1;
+                    lblGuessed.Text = "Invitado: " + guessed + " veces.";
+                }
+                e.Handled = true; // Para evitar alertar alertar al sistema y que no suene
+            }
+        }
+
+        private void Setup() // Metodo que cargara las palabras de la lista
+        {
+            words = File.ReadLines("words.txt").ToList(); // Reccore las palabras de words.txt
+            newText = Scramble(words[i]); // Metodo que desordenara las palabras en el array
+            lblWord.Text = newText; // Le agrega al label las palabras 
+            lblInfo.Text = "Palabras: " + (i + 1) + " de " + words.Count; // Le agrega al otro label el numero de la palabra usada
+        }
+
+        /* La función scramble rompe la palabra en letras, le asigna a cada letra un "número aleatorio" temporal (un Guid),
+           ordena las letras basándose en esos números aleatorios y, finalmente, vuelve a unir las letras desordenadas para formar la nueva palabra. */
+        private string Scramble(string text) // Metodo que desordenara las palabras de la lista
+        {
+            return new string(text.ToCharArray().OrderBy(x => Guid.NewGuid()).ToArray());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
