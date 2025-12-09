c1124e02de330d9f53ae9fb079b2ef710b70a33c
HectorMMZ <zacrosgamer29@gmail.com>
2025-12-09 00:37:18 -0600

    Add files via upload
    modificacion para agregar un menu y pequena bd para puntuaciones

---
+++ MenuForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Proyecto_Adivina_la_Palabra
+{
+    partial class MenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombreUsuario = new System.Windows.Forms.TextBox();
+            this.btnJugar = new System.Windows.Forms.Button();
+            this.btnVerPuntuaciones = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(167, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(421, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ADIVINA LA PALABRA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(320, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresa tu nombre";
+            // 
+            // txtNombreUsuario
+            // 
+            this.txtNombreUsuario.Location = new System.Drawing.Point(348, 354);
+            this.txtNombreUsuario.Name = "txtNombreUsuario";
+            this.txtNombreUsuario.Size = new System.Drawing.Size(100, 22);
+            this.txtNombreUsuario.TabIndex = 2;
+            // 
+            // btnJugar
+            // 
+            this.btnJugar.Location = new System.Drawing.Point(136, 254);
+            this.btnJugar.Name = "btnJugar";
+            this.btnJugar.Size = new System.Drawing.Size(75, 23);
+            this.btnJugar.TabIndex = 3;
+            this.btnJugar.Text = "JUGAR";
+            this.btnJugar.UseVisualStyleBackColor = true;
+            // 
+            // btnVerPuntuaciones
+            // 
+            this.btnVerPuntuaciones.Location = new System.Drawing.Point(338, 254);
+            this.btnVerPuntuaciones.Name = "btnVerPuntuaciones";
+            this.btnVerPuntuaciones.Size = new System.Drawing.Size(110, 23);
+            this.btnVerPuntuaciones.TabIndex = 4;
+            this.btnVerPuntuaciones.Text = "PUNTUACIONES";
+            this.btnVerPuntuaciones.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(527, 254);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // MenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnVerPuntuaciones);
+            this.Controls.Add(this.btnJugar);
+            this.Controls.Add(this.txtNombreUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MenuForm";
+            this.Text = "MenuForm";
+            this.Load += new System.EventHandler(this.MenuForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void MenuForm_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombreUsuario;
+        private System.Windows.Forms.Button btnJugar;
+        private System.Windows.Forms.Button btnVerPuntuaciones;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
---
+++ MenuForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Proyecto_Adivina_la_Palabra
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJugar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombreUsuario.Text))
+            {
+                MessageBox.Show("¡Por favor ingresa un nombre para jugar!", "Atención");
+                return;
+            }
+
+            // Ocultamos el menú y abrimos el juego pasando el nombre del jugador
+            Form1 juego = new Form1(txtNombreUsuario.Text);
+            this.Hide();
+            juego.ShowDialog(); // Abre el juego y espera a que se cierre
+            this.Show(); // Vuelve a mostrar el menú cuando el juego termina
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnVerPuntuaciones_Click(object sender, EventArgs e)
+        {
+            // Aquí mostraremos la "base de datos" de puntuaciones
+            try
+            {
+                if (System.IO.File.Exists("puntuaciones.txt"))
+                {
+                    string scores = System.IO.File.ReadAllText("puntuaciones.txt");
+                    MessageBox.Show(scores, "Mejores Puntuaciones");
+                }
+                else
+                {
+                    MessageBox.Show("Aún no hay puntuaciones registradas.", "Vacío");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al leer puntuaciones: " + ex.Message);
+            }
+        }
+    }
+}
